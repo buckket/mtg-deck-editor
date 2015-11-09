81d1e86a85442897be3a7e222f4cbcf083df10c7
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2015-11-09 19:06:25 +0100

    + search entry and card scraping

--- mtg-deck-editor.py
+++ mtg-deck-editor.py
@@ -20,8 +20,15 @@
 
 from __future__ import with_statement
 
+from gi.repository import Gio
 from gi.repository import Gtk
+from gi.repository.GdkPixbuf import Pixbuf
+
+from requests import get
+from html5lib import parse
+
 import os
+import re
 
 class MtgDeckEditor:
     def __init__(self):
@@ -30,6 +37,7 @@ class MtgDeckEditor:
         builder.connect_signals(self)
 
         self.window = builder.get_object("window")
+        self.image_card = builder.get_object("image_card")
 
     def main(self):
         self.window.show_all()
@@ -37,6 +45,60 @@ class MtgDeckEditor:
 
     def on_window_destroy(self, widget, data=None):
         Gtk.main_quit()
+
+    def on_searchentry_activate(self, widget, data=None):
+        query = widget.get_text()
+        card = Card(query)
+        self.image_card.set_from_pixbuf(card.pixbuf)
+        print card
+
+class Card:
+    def __init__(self, query):
+        image_url = \
+            "http://gatherer.wizards.com/Handlers/Image.ashx?type=card&name=%s" % \
+            query
+        # rotate split cards
+        if '//' in query:
+            image_url= '%s&options=rotate90' % image_url
+        print image_url
+        image_raw = get(image_url).content
+        input_stream = Gio.MemoryInputStream.new_from_data(image_raw, None)
+        self.pixbuf = Pixbuf.new_from_stream(input_stream, None)
+
+        # handle split cards
+        query = re.sub("(.*) // (.*)", r"[\1]+[//]+[\2]", query)
+        html_url = \
+            "http://gatherer.wizards.com/Pages/Card/Details.aspx?name=%s" % \
+            query
+        html = get(html_url).text
+        self.dom = parse(html, treebuilder='etree', namespaceHTMLElements=False)
+
+    @property
+    def name(self):
+        xpath = ".//*[@id='ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_" + \
+            "%sRow']/div" % 'name'
+        return self.dom.findall(xpath)[1].text.strip()
+
+    @property
+    def type(self):
+        xpath = ".//*[@id='ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_" + \
+            "%sRow']/div" % 'type'
+        return self.dom.findall(xpath)[1].text.strip()
+
+    @property
+    def cmc(self):
+        xpath = ".//*[@id='ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_" + \
+            "%sRow']/div" % 'cmc'
+        return self.dom.findall(xpath)[1].text.strip()
+
+    @property
+    def text(self):
+        xpath = ".//*[@id='ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_" + \
+            "%sRow']/div/div" % 'text'
+        return self.dom.findall(xpath)[0].text.strip()
+
+    def __str__(self):
+        return "%s | %s | %s | %s" % (self.name, self.type, self.cmc, self.text)
 
 if __name__ == '__main__':
     mde = MtgDeckEditor()
