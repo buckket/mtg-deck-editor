8abb855af5ecfd7b80cae072093d895e4a73f7be
buckket <buckket@cock.li>
2015-11-18 17:16:30 +0100

    + added setup.py

---
+++ setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup
+
+setup(
+    name='mtg-deck-editor',
+    version='1.0.0',
+
+    url='https://github.com/buckket/mtg-deck-editor',
+
+    author='buckket',
+    author_email='buckket@cock.li',
+
+    py_modules=['mtg_deck_editor'],
+
+    zip_safe=True,
+    include_package_data=True,
+
+    platforms='any',
+
+    install_requires=[
+        'requests',
+        'requests-cache',
+        'html5lib',
+        'matplotlib',
+        'PyGObject',
+        'setuptools',
+        'pip'
+    ],
+
+    entry_points={
+        'gui_scripts': [
+            'mtg-deck-editor = mtg_deck_editor:main',
+        ]
+    },
+
+    description='A GUI deck editor for the card game Magic: The Gathering.',
+    long_description=open('./README.rst', 'r').read(),
+
+    keywords=['mtg', 'deck editor', 'card game'],
+
+    license='GPLv3',
+    classifiers=[
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Games/Entertainment',
+    ],
+)
